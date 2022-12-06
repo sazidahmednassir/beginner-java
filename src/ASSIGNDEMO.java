56c01b73e393923e0e8d762616fdac54b8d2e1e2
unknown <sazidahmednassir@gmail.com>
2022-12-06 07:21:07 -0800

    assignment opaertor

---
+++ src/ASSIGNDEMO.java
@@ -0,0 +1,17 @@
+public class ASSIGNDEMO {
+    public static void main(String[] args) {
+        int x=2;
+        int y=5;
+
+        x+=y;
+        System.out.println("x : "+x);
+        x-=y;
+        System.out.println("x : "+x);
+        x*=y;
+        System.out.println("x : "+x);
+        x/=y;
+        System.out.println("x : "+x);
+        x%=y;
+        System.out.println("x : "+x);
+    }
+}
